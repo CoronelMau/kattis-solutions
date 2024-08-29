4b9c5f7d365d78ea9292c0804cb2ef520c7bbc7b
unknown <8mau.coronel@gmail.com>
2024-08-28 18:25:21 -0600

    Kattis - FizzBuzz Solution

---
+++ fizzbuzz.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int x, y, n;
+  cin >> x >> y >> n;
+
+  for (int i = 1; i <= n; i++)
+  {
+    if (i % x == 0)
+    {
+      cout << "Fizz";
+    }
+    if (i % y == 0)
+    {
+      cout << "Buzz";
+    }
+    else if (i % x != 0 && i % y != 0)
+    {
+      cout << i;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
